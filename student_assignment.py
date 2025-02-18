5fab4561564e5a77d148470a29bc682a7c66e2ac
q098756 <q098756@gmail.com>
2025-02-18 16:06:47 +0800

    Implement hw02 v1

--- student_assignment.py
+++ student_assignment.py
@@ -7,7 +7,21 @@ q2_pdf = "勞動基準法.pdf"
 
 
 def hw02_1(q1_pdf):
-    pass
+    return PyPDFLoader(q1_pdf).load()
 
 def hw02_2(q2_pdf):
-    pass
+    loader = PyPDFLoader(q2_pdf)
+    docs = loader.load()
+    text = ""
+    for doc in docs:
+        text = text + doc.page_content + "\n"
+    spliter = RecursiveCharacterTextSplitter(separators=["第 \\d+-?\\d* 條\n|第 .* 章"],
+                                    chunk_size=0,
+                                    chunk_overlap=0,
+                                    is_separator_regex=True,
+                                    keep_separator=True)
+    result = spliter.create_documents([text])
+    for index, doc in enumerate(result):
+        print(index)
+        print(doc.page_content)
+    return len(result)
